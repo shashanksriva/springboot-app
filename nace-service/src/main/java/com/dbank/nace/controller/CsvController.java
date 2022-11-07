ced8fcc4624c543fcf88c85c37d00fd6bf515d6c
shashanksriva <shashanksriva@gmail.com>
2022-11-07 16:32:26 -0500

    Fixing request mapping for csv upload

--- nace-service/src/main/java/com/dbank/nace/controller/CsvController.java
+++ nace-service/src/main/java/com/dbank/nace/controller/CsvController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Api(tags="CSV handler",
         description = "Provides API to perform CSV related operations")
 @Controller
-//@RequestMapping("/api/csv")
+@RequestMapping("/csv")
 @Slf4j
 public class CsvController {
 
